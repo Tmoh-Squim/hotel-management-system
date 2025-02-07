575810012c85fb878b7d76769c11390d31515ab3
Tmoh-Squim <timoohwilliams885@gmail.com>
2025-02-07 20:56:40 +0300

    updated

--- app/components/HotelCard.tsx
+++ app/components/HotelCard.tsx
@@ -5,7 +5,7 @@ import { ProductProps } from '../types/types'
 const HotelCard = ({product}:ProductProps) => {
   return (
     <div className='block gap-3'>
-        <Image width={250} height={250} src={product.image} alt={product.title}/>
+        <Image width={250} height={250} src={product.images[0]} alt={product.title}/>
         
     </div>
   )
