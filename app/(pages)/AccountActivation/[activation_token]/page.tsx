ad686eee037453416b4f8c677851cd61368fb90c
Tmoh-Squim <timoohwilliams885@gmail.com>
2025-02-17 14:23:28 +0300

    updated

--- app/(pages)/AccountActivation/[activation_token]/page.tsx
+++ app/(pages)/AccountActivation/[activation_token]/page.tsx
@@ -44,7 +44,7 @@ const SellerActivationPage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       {loading ? (
-    <div className={`animate-spin  h-15 w-15 border-4 border-blue-500 border-t-transparent rounded-full`}></div>
+    <div className={`animate-spin  h-10 w-10 border-4 border-blue-500 border-t-transparent rounded-full`}></div>
   ) : error ? (
         <p className="text-red-400 text-xl">{message}</p>
       ) : (
